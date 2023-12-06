b953c7a28ff6094f670f140674a3a83fb67d22db
Mehmet Nazliay <65411256+mhmtnzly@users.noreply.github.com>
2023-12-07 00:23:54 +0100

    update the code

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,8 +4,6 @@ import './Header.css'
 
 export default function Header() {
     return (
-        <header className='header'>
-            <Navbar />
-        </header>
+            <Navbar />  
     );
 }
--- src/components/ImageSlider/ImageSlider.jsx
+++ src/components/ImageSlider/ImageSlider.jsx
@@ -1,26 +1,34 @@
-import React, { useEffect, useState } from 'react';
-import './ImageSlider.css'
+import React, { useEffect, useState } from "react";
+import "./ImageSlider.css";
 
 const ImageSlider = (props) => {
-    const [currentImageIndex, setCurrentImageIndex] = useState(0);
-    const changeImage = () => {
-        setCurrentImageIndex((prevIndex) => (prevIndex + 1) % props.images.length);
-    };
-    useEffect(() => {
-        const interval = setInterval(changeImage, 2000);
+  const [currentImageIndex, setCurrentImageIndex] = useState(0);
+  const [opacity, setOpacity] = useState(1);
 
-        // Clean up the interval on component unmount
-        return () => clearInterval(interval);
-    }, []);
-    return (
-        <div className="image-container">
-            <img
-                className="image-slider"
-                src={props.images[currentImageIndex]}
-                alt='test-resim'
-            />
-        </div>
-    );
+  const changeImage = () => {
+    setOpacity(0); // Geçiş başladığında opacity'yi sıfıra ayarla
+
+    setTimeout(() => {
+      setCurrentImageIndex((prevIndex) => (prevIndex + 1) % props.images.length);
+      setOpacity(1); // Yeni resimde opacity'yi bir yap
+    }, 10000); // Timeout süresi, geçişin tamamlanma süresinden kısa olmalı
+  };
+
+  useEffect(() => {
+    const interval = setInterval(changeImage, 3000);
+    return () => clearInterval(interval);
+  }, []);
+
+  return (
+    <div className="container">
+      <img
+        className={`bg-image ${currentImageIndex === 0 ? "show" : ""}`}
+        style={{ opacity: opacity }}
+        src={props.images[currentImageIndex]}
+        alt={`${currentImageIndex + 1}`}
+      />
+    </div>
+  );
 };
 
 export default ImageSlider;

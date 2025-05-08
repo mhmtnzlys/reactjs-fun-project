556d58778329d021a54487410f3dfe50183a3480
Mehmet Nazliay <124599671+mhmtnzlys@users.noreply.github.com>
2025-05-08 13:32:48 +0000

    update frontned

--- src/App.js
+++ src/App.js
@@ -1,19 +1,19 @@
 import logo from './logo.svg';
-import React, { useState, useEffect } from 'react';
+// import React, { useState, useEffect } from 'react';
 import './App.css';
 
 function App() {
-  const [data, setData] = useState(null);
+  // const [data, setData] = useState(null);
 
 
 
-  useEffect(() => {
-    fetch('http://localhost:5000/api')
-      .then(response => response.json())
-      .then(data => setData(data))
-      .catch(error => console.error('Error:', error));
-  }, []);
-console.log(data);
+//   useEffect(() => {
+//     fetch('http://localhost:5000/api')
+//       .then(response => response.json())
+//       .then(data => setData(data))
+//       .catch(error => console.error('Error:', error));
+//   }, []);
+// console.log(data);
 
   return (
     <div className="App">
